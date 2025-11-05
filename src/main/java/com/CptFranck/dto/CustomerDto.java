2711819139a0af7cea3fe8482011a85ae35a92a0
CptFranck <francois.caillet71@gmail.com>
2025-11-05 15:34:58 +0100

    update CustomerDto with string id type

--- src/main/java/com/CptFranck/dto/CustomerDto.java
+++ src/main/java/com/CptFranck/dto/CustomerDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDto {
-    private Long id;
+    private String id;
 
     private String username;
 
